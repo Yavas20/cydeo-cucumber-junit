f1797687c066feb217384a6af0d18ffffcac6d5f
Yavas20 <97404892+Yavas20@users.noreply.github.com>
2022-06-18 22:12:32 +0300

    cucumber - takeScreenShot - paremeterization - data tables - scenarioOutlines

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -16,6 +17,14 @@ public class GoogleSearchPage {
 
     @FindBy(xpath = "//input[@name='q']")
     public WebElement googleSearchBox;
+
+    //    public WebElement capitalElement(String capital){
+//        return Driver.getDriver().findElement(By.linkText(capital));
+//    }
+
+    public String getCapital(){
+        return Driver.getDriver().findElement(By.xpath("//a[@class='FLP8od']")).getText();
+    }
 
 
 
